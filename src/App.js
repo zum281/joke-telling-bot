db10a90ce55236d2721b5962c50b2d7014a56ce9
Fulvia <32518183+zum281@users.noreply.github.com>
2021-08-23 11:25:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [joke, setJoke] = useState({ setup: null, punchline: null });
+    const [loading, setLoading] = useState(false);
+    const resetJoke = () => {
+        setJoke({ setup: null, punchline: null });
+    };
+
+    const getJoke = () => {
+        resetJoke();
+        setLoading(true);
+
+        fetch("https://official-joke-api.appspot.com/jokes/random")
+            .then((res) => res.json())
+            .then((data) => {
+                let newJoke = {
+                    setup: data.setup,
+                    punchline: data.punchline,
+                };
+                setJoke({ ...newJoke });
+                setLoading(false);
+            })
+            .catch((err) => {
+                setLoading(false);
+                console.log(err);
+            });
+    };
+
+    return (
+        <div className="root">
+            {joke && (
+                <div>
+                    <p>{joke.setup}</p>
+                    <p>{joke.punchline}</p>
+                </div>
+            )}
+            {loading && <p>loading...</p>}
+            <button onClick={getJoke}>Tell me a joke</button>
+        </div>
+    );
 }
 
 export default App;
